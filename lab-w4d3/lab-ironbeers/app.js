2af10b7e81e1fbced1b29b70c6b3486bfd578daa
Hernando Bifaretti <hbifaretti@gmail.com>
2022-06-23 16:07:48 +0200

    done

---
+++ lab-w4d3/lab-ironbeers/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const hbs = require('hbs');
+const path = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const app = express();
+const punkAPI = new PunkAPIWrapper();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+// ...
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers.hbs', { beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      let beer = randomBeer[0];
+      res.render('random-beer.hbs', beer);
+      // console.log(beer);
+    })
+
+    .catch(error => console.log(error));
+});
+
+// Add the route handlers here:
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'));
+//
